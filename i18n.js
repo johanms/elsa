a8cf2c7cedfd5e18f8625b5630e20d732e30dc07
Johan Markus Steinholt <johanms@Shakys-MacBook-Air.local>
2019-06-25 22:06:21 +0200

    Move translations to separate files

--- i18n.js
+++ i18n.js
@@ -1,74 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-// the translations
-// (tip move them in a JSON file and import them)
+import common_en from "./translations/common_en.json";
+import common_no from "./translations/common_no.json";
+
 const resources = {
   en: {
-    translation: {
-      currentTime: "Current time is",
-      remember: "Remember",
-
-      d0: "monday",
-      d1: "tuesday",
-      d2: "wednesday",
-      d3: "thursday",
-      d4: "friday",
-      d5: "saturday",
-      d6: "sunday",
-
-      m0: "january",
-      m1: "february",
-      m2: "march",
-      m3: "april",
-      m4: "may",
-      m5: "june",
-      m6: "july",
-      m7: "august",
-      m8: "september",
-      m9: "october",
-      m10: "november",
-      m11: "december",
-
-      night: "night",
-      morning: "morning",
-      beforeDinner: "before dinner",
-      afternoon: "afternoon",
-      evening: "evening"
-    }
+    translation: common_en
+  },
+  en_US: {
+    translation: common_en
+  },
+  en_GB: {
+    translation: common_en
+  },
+  nb: {
+    translation: common_no
   },
   nb_NO: {
-    translation: {
-      currentTime: "Klokken er",
-      remember: "Husk",
-
-      d0: "mandag",
-      d1: "tirsdag",
-      d2: "onsdag",
-      d3: "torsdag",
-      d4: "fredag",
-      d5: "lørdag",
-      d6: "søndag",
-
-      m0: "januar",
-      m1: "februar",
-      m2: "mars",
-      m3: "april",
-      m4: "mai",
-      m5: "juni",
-      m6: "juli",
-      m7: "august",
-      m8: "september",
-      m9: "oktober",
-      m10: "november",
-      m11: "desember",
-
-      night: "natt",
-      morning: "morgen",
-      beforeDinner: "formiddag",
-      afternoon: "ettermiddag",
-      evening: "kveld"
-    }
+    translation: common_no
   }
 };
 
